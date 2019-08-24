35f0ac53e8dac9a32c5e21cda3a1a2680c70692d
Dalson Salvador de Araujo <dalsonn@gmail.com>
2019-08-24 15:49:26 -0300

    Add files via upload

---
+++ JWTWebApi/JWTWebApi/Startup.cs
@@ -0,0 +1,44 @@
+﻿using AuthorizationServer.Api.Formats;
+using AuthorizationServer.Api.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+namespace AuthorizationServer.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            //Web API routes
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                //RefreshTokenProvider = new RefreshTokenProvider(),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:44391/")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+    }
+}
